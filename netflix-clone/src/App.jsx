4858a645637853392c3a12750eac829241e396da
LamesginewAndargie <lame2002@gmail.com>
2024-11-10 14:46:37 +0300

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello Netlixxxxx</h1>
+			<Home />
 		</div>
 	);
 }
